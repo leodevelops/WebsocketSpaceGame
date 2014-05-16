a1b18b2e012778496d9e15e344815c124634224d
Lev Rozenberg <leodevelops@gmail.com>
2014-05-16 14:38:55 -0400

    rewrote event queue using lock and condition

--- src/spacegame/GameEventQueue.java
+++ src/spacegame/GameEventQueue.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,26 +14,36 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class GameEventQueue implements Runnable {
     private final Queue<Runnable> events;
-    private ScheduledExecutorService executorService;
+    private final ScheduledExecutorService executorService;
+    private final Lock lock;
+    private final Condition eventsEmpty;
 
     public GameEventQueue() {
         events = new LinkedList<Runnable>();
         executorService = new ScheduledThreadPoolExecutor(4);
+        lock = new ReentrantLock();
+        eventsEmpty = lock.newCondition();
     }
 
     public void execute(Runnable event) {
-        synchronized (events) {
+        lock.lock();
+        try {
             events.add(event);
-            events.notify();
+            eventsEmpty.signal();
+        } finally {
+            lock.unlock();
         }
     }
 
     public void executeAll(Runnable[] events) {
-        synchronized (this.events) {
+        lock.lock();
+        try {
             for(Runnable r : events) {
                 this.events.add(r);
             }
-            this.events.notify();
+            eventsEmpty.signal();
+        } finally {
+            lock.unlock();
         }
     }
 
@@ -59,16 +70,16 @@ public class GameEventQueue implements Runnable {
     @Override
     public void run() {
         while(true) {
-            synchronized(events) {
-                if(events.isEmpty()) {
-                    try {
-                        events.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    events.remove().run();
+            lock.lock();
+            try {
+                while(events.isEmpty()) {
+                    eventsEmpty.await();
                 }
+                events.remove().run();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
             }
         }
     }
